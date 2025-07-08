cc9f4fc3e6bbc8bf4be448a22a82eaabc294b04d
Fabio Freitas <fhcfreitas@gmail.com>
2025-07-07 22:15:32 -0300

    validations to game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,6 @@
 class Game < ApplicationRecord
+  validates :name, presence: true
+  validates :publisher, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :release_year, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1950 }
 end
